1e01dfb85045b287268ca63fa62547421f810efe
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2023-08-23 08:13:12 -0600

    original romania graph example

--- cmd/searchAlgorithms.go
+++ cmd/searchAlgorithms.go
@@ -1,77 +1,22 @@
 package main
 
 import (
-	"aiSearchAlgorithms/internal/graph"
-	"aiSearchAlgorithms/internal/graphbuilder"
 	"aiSearchAlgorithms/pkg/algorithms"
+	"aiSearchAlgorithms/testdata"
 	"fmt"
 )
 
 func main() {
 
-	b := new(graphbuilder.GraphBuilder)
-	b.WithGraphOrder(5)
-	b.WithEdges(map[graph.Edge]int{
-		{U: 0, V: 1}: 80,
-		{U: 0, V: 2}: 99,
-		{U: 1, V: 3}: 97,
-		{U: 2, V: 4}: 211,
-		{U: 3, V: 4}: 101,
-	})
-	g := b.Build(graph.WeightedMatrix)
-	//g := testdata.GetTestGraph(testdata.GraphRomania0)
+	g := testdata.GetTestGraph(testdata.GraphRomania)
 
 	vertices := g.Vertices()
 
 	//setting the goal
-	vertices[4].Value = 1
+	vertices[12].Value = 1
 
-	/*
-		// =================== DFS ================
-		fmt.Println("\nDFS execution:")
-		answer := algorithms.DFS(g, vertices[0], 1)
-
-		fmt.Println("\nVertices after DFS:")
-		for _, v := range vertices {
-			fmt.Println(v)
-		}
-
-		fmt.Println("\nPath from root:")
-
-		path := algorithms.Path(answer)
-		for i := len(path) - 1; i >= 0; i-- {
-			fmt.Println(path[i])
-		}
-
-		fmt.Println("\nTotal path weight:")
-		fmt.Println(algorithms.PathWeight(answer, g.Edges()))
-
-		// =================== DFS ================
-		fmt.Println("\nBFS execution:")
-		answer = algorithms.BFS(g, vertices[0], 1)
-
-		vertices = g.Vertices()
-
-		fmt.Println("\nVertices after BFS:")
-		for _, v := range vertices {
-			fmt.Println(v)
-		}
-
-		fmt.Println("\nPath from root:")
-
-		path = algorithms.Path(answer)
-		for i := len(path) - 1; i >= 0; i-- {
-			fmt.Println(path[i])
-		}
-
-		fmt.Println("\nTotal path weight:")
-		fmt.Println(algorithms.PathWeight(answer, g.Edges()))
-
-	*/
-
-	// =================== IDS ================
-	fmt.Println("\nIDS execution:")
-	answer := algorithms.UCS(g, vertices[0], 1)
+	fmt.Println("\nUCS execution:")
+	answer := algorithms.IDS(g, vertices[0], 1)
 
 	vertices = g.Vertices()
 
@@ -92,5 +37,5 @@ func main() {
 
 	g.SetHeuristic(map[int]float64{0: 42.42})
 
-	fmt.Println(g.Heuristic(vertices[0]))
+	//fmt.Println(g.Heuristic(vertices[0]))
 }
--- testdata/testGraphs.go
+++ testdata/testGraphs.go
@@ -1,6 +1,9 @@
 package testdata
 
-import "aiSearchAlgorithms/internal/graph"
+import (
+	"aiSearchAlgorithms/internal/graph"
+	"aiSearchAlgorithms/internal/graphbuilder"
+)
 
 type GraphName string
 
@@ -9,6 +12,7 @@ const (
 	Graph07W      GraphName = "Graph07W"
 	Graph07WDFS   GraphName = "Graph07WDFS"
 	GraphRomania0 GraphName = "GraphRomania0"
+	GraphRomania  GraphName = "GraphRomania"
 )
 
 func GetTestGraph(name GraphName) graph.Graph {
@@ -59,6 +63,35 @@ func GetTestGraph(name GraphName) graph.Graph {
 			{0, 0, 211, 101, 0},
 		}
 		implementation = graph.WeightedMatrix
+	case GraphRomania:
+		b := new(graphbuilder.GraphBuilder)
+		b.WithGraphOrder(20)
+		b.WithEdges(map[graph.Edge]int{
+			{U: 0, V: 1}:   118,
+			{U: 0, V: 2}:   75,
+			{U: 2, V: 3}:   71,
+			{U: 0, V: 5}:   140,
+			{U: 1, V: 4}:   111,
+			{U: 3, V: 5}:   151,
+			{U: 4, V: 6}:   70,
+			{U: 5, V: 7}:   80,
+			{U: 5, V: 8}:   99,
+			{U: 6, V: 9}:   75,
+			{U: 9, V: 10}:  120,
+			{U: 7, V: 10}:  146,
+			{U: 7, V: 11}:  97,
+			{U: 8, V: 12}:  211,
+			{U: 11, V: 12}: 101,
+			{U: 12, V: 13}: 90,
+			{U: 12, V: 14}: 85,
+			{U: 14, V: 15}: 98,
+			{U: 15, V: 17}: 86,
+			{U: 14, V: 16}: 142,
+			{U: 16, V: 18}: 92,
+			{U: 18, V: 19}: 87,
+		})
+		g := b.Build(graph.WeightedMatrix)
+		return g
 	}
 
 	switch implementation {
