752185884119c47bbe0a8900d83493c717958269
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2023-08-24 14:58:00 -0600

    graph. go comments

--- internal/graph/graph.go
+++ internal/graph/graph.go
@@ -1,5 +1,7 @@
+// Package graph provides an interface and several implementations for graphs.
 package graph
 
+// Vertex represent a graph vertex.
 type Vertex struct {
 	Name     string
 	Id       int
@@ -11,6 +13,7 @@ type Vertex struct {
 	Value    int
 }
 
+// Returns a new initialized vertex, receives its id as an integer.
 func NewVertex(id int) *Vertex {
 	v := new(Vertex)
 	v.Id = id
@@ -21,15 +24,31 @@ func NewVertex(id int) *Vertex {
 	return v
 }
 
+// Edge defines a graph edge through the ids of it two ends.
 type Edge struct {
 	U int
 	V int
 }
 
+// Map with graph edges keys and weights values.
 type WeightedEdges map[Edge]int
+
+// Map with vertex ids keys and graph vertices values.
 type Vertices map[int]*Vertex
+
+// Map with vertex ids keys and heuristic values.
 type Heuristic map[int]int
 
+// Enum to define the different graph implementations.
+type GraphImplementation string
+
+const (
+	Matrix         GraphImplementation = "Matrix Graph"
+	WeightedMatrix GraphImplementation = "Weighted Matrix Graph"
+)
+
+// Graph interface. Defines the basic desired behaviour for a graph in order to
+// be able to perform searches over it.
 type Graph interface {
 	Neighbours(*Vertex) []*Vertex
 	Vertices() Vertices
@@ -38,10 +57,3 @@ type Graph interface {
 	Heuristic(*Vertex) int
 	SetHeuristic(Heuristic)
 }
-
-type GraphImplementation string
-
-const (
-	Matrix         GraphImplementation = "Matrix Graph"
-	WeightedMatrix GraphImplementation = "Weighted Matrix Graph"
-)
